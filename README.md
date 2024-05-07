bd21a1feb5a201d43ffd2f756e6d01abbebead30
glee <g.lee13770334@gmail.com>
2024-05-06 20:09:56 -0400

    feature -- initial html/css/js framework (handles resizing)

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # zensui
+A zig web ui framework.
+
+## Build
+    $ zig build-exe -target wasm32-freestanding src/main.zig -fno-entry -rdynamic
--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,25 @@
 const std = @import("std");
 
-pub fn main() !void {
-    std.debug.print("Hello, World!\n", .{});
+extern fn jsPrintText(source: [*]const u8, len: c_uint, x: c_uint, y: c_uint) void;
+extern fn jsStrokeRect(x: c_uint, y: c_uint, width: c_uint, height: c_uint) void;
+
+fn printText(text: []const u8, x: u32, y: u32) void {
+    jsPrintText(text.ptr, text.len, x, y);
+}
+
+const State = struct {
+    width: u32,
+    height: u32,
+};
+
+var state: State = .{ .width = 0, .height = 0 };
+
+export fn onInit() void {
+    jsStrokeRect(0, 0, state.width, state.height);
+    printText("Hello World!", state.width / 2, state.height / 2);
+}
+
+export fn onResize(width: c_uint, height: c_uint) void {
+    state = .{ .width = width, .height = height };
+    onInit();
 }
